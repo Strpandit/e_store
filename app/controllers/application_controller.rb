09d2c954cc7e3d5b3c599ea6a1a12141d407f2be
Ashukla1998 <shuklaashu134@gmail.com>
2023-12-01 12:59:11 +0530

    user authentication using devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
+
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
+
+    def after_sign_in_path_for(resource)
+        root_path # Replace with your desired path after sign in
+    end
+
+    def after_sign_up_path_for(resource)
+        new_user_session_path # Redirect to sign in after sign up
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        root_path # Redirect to root after sign out
+    end
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    end
+  end
+  
